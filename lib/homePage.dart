ecafaf3edb5957e4b2a921fc991417a47c994587
Kalpana <kalpana@foodmonk.com>
2020-08-15 05:10:10 -0700

    added home page

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +9,68 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row( mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.notifications_none, color: Colors.white,),
+                    onPressed: (){
+
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.search, color: Colors.white,),
+                    onPressed: (){
+
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.account_circle, color: Colors.white,),
+                    onPressed: (){
+
+                    },
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.blueGrey[900],
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text("SENSEX",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          subtitle: Text("23416.43",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        Row( mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(DateTime.now().toString())
+                          ],
+                        )
+                      ],
+                    ),
+
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
